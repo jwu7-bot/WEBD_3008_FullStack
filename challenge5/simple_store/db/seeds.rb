3809e3301afc9cec463517be330c1d5dc0b530d3
JiaHui Wu <jwu7@rrc.ca>
2024-10-16 00:59:44 -0500

    Rewrote the db/seeds.rb file such that it now pulls in product and category data from products.csv

--- challenge5/simple_store/db/seeds.rb
+++ challenge5/simple_store/db/seeds.rb
@@ -1,13 +1,33 @@
 require "csv"
 
-Product.delete_all
+Product.destroy_all
+Category.destroy_all
 
-676.times do
-  Product.create(
-    title: Faker::Commerce.product_name,
-    price: Faker::Commerce.price(range: 0..100.0),
-    stock_quantity: Faker::Number.between(from: 1, to: 100)
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true)
+
+# If CSV was created by Excel in Windows you may also need to set an encoding type:
+# products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+products.each do |product|
+  # Create categories and products here.
+  category_name = product["category"]
+  title = product["name"]
+  description = product["description"]
+  price = product["price"]
+  stock_quantity = product["stock quantity"]
+
+  category = Category.find_or_create_by(name: category_name)
+
+  Product.create!(
+    title: title,
+    description: description,
+    price: price,
+    stock_quantity: stock_quantity,
+    category: category
   )
 end
 
-puts "Created #{Product.count} Products."
+puts "Created #{Product.count} Products"
