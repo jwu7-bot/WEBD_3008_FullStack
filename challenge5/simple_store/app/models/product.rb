88900bc33491bab41c1107ff1b3dc81239007884
JiaHui Wu <jwu7@rrc.ca>
2024-10-15 23:46:10 -0500

    Added validation to the Product model to ensure that the product always includes a title, price and stock_quantity

--- challenge5/simple_store/app/models/product.rb
+++ challenge5/simple_store/app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :title, presence: true
+  validates :price, presence: true, numericality: { greatter_than_or_equal_to: 0 }
+  validates :stock_quantity, presence: true, numericality: { only_integer: true, greatter_than_or_equal_to: 0 }
 end
