008ce2dc052f32ddf453934b87813870a4e2407f
JiaHui Wu <jwu7@rrc.ca>
2024-11-04 19:52:49 -0600

    Updated the Customer model to attach an image, added validations, and modified the active admin config for customer to allow image uploads.

--- challenge6/crm_challenge/app/admin/customers.rb
+++ challenge6/crm_challenge/app/admin/customers.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Customer do
   permit_params :full_name, :phone_number, :email_address, :notes
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+     f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+    end
+    f.actions
+  end
 end
