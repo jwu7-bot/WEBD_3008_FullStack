7bfe2ad161225cdbefaa0f9ebf64a66de4097e0a
JiaHui Wu <jwu7@rrc.ca>
2024-11-07 03:02:23 -0600

    Added link to the other pages. Updated the routes.

--- challenge6/crm_challenge/config/routes.rb
+++ challenge6/crm_challenge/config/routes.rb
@@ -1,21 +1,11 @@
 Rails.application.routes.draw do
-  resources :customers, only: [ :index ]
-  resources :customers do
-    collection do
-      get :alphabetized
-      get :missing_email
-    end
-  end
-
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  root "customers#index"
 
-  # Render dynamic PWA files from app/views/pwa/*
-  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "/customers/missing_email", to: "customers#missing_email", as: "missing_email"
+  get "/customers/alphabetized", to: "customers#alphabetized", as: "alphabetized"
+
+  resources :customers
 end
